59e1c85895e05c474f360b7ade2e6082251d019a
veeriyaperumal7 <148101491+veeriyaperumal7@users.noreply.github.com>
2024-02-06 00:06:50 +0530

    Level 2 complete

--- src/com/veeriyaperumal/game/GameView.java
+++ src/com/veeriyaperumal/game/GameView.java
@@ -1,5 +1,8 @@
 package com.veeriyaperumal.game;
 
+import java.util.Queue;
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.Random;
 
 import com.veeriyaperumal.boarddata.BoardDataView;
@@ -18,17 +21,51 @@ public class GameView {
 	public void start() {
 		boardDataView.collectGameData();
 		startGame();
+		startGameLevel2();
+	}
+
+	private void startGameLevel2() {//Finding minimum path to reach....
+		int targetNumber = gameViewModel.getTargetPositionNumber();
+		
+		Queue<int[]> queue = new LinkedList<int[]>();
+		queue.add(new int[]{0,0});
+		while(!queue.isEmpty()) {
+			int[] arr = queue.poll();
+			int currentPosition=arr[0];
+			int count=arr[1];
+			
+			if(currentPosition==targetNumber) {
+				System.out.println("The minimum path to reach finish point is : "+ count);
+				return;
+			}
+			
+			for(int i=1;i<=6;i++) {
+				
+				if(gameViewModel.isSnake(currentPosition+i)!=-1) {
+					continue;
+				}
+				int ladder = gameViewModel.isLadder(currentPosition+i);
+				if(ladder!=-1) {
+					queue.add(new int[] {ladder,count+1});
+				}else if(currentPosition+i<=targetNumber) {
+					queue.add(new int[] {currentPosition+i,count+1});
+				}
+			}
+			
+		}
+		
 	}
 
 	private void startGame() {
-
+          
 		Player[] players = gameViewModel.getPlayers();
 		int targetNumber = gameViewModel.getTargetPositionNumber();
 		System.out.println("Game was started : ");
 		Random random = new Random();
-		int diceResult, snakeBiteValue, ladderValue;
+		int diceResult, snakeBiteValue, ladderValue,consecutiveCount=0;
 		outerLoop: while (true) {
-			for (Player player : players) {
+			for(int i=0;i<players.length;i++) {
+				Player player=players[i];
 				System.out.println(player.getName() + " is playing rolling a dice.");
 				diceResult = random.nextInt(6);
 				System.out.println("Dice number was : " + diceResult);
@@ -54,6 +91,14 @@ public class GameView {
 					System.out.println("Result : That is a safe move....");
 				}
 				System.out.println("===========================================");
+				if(diceResult==6) { // If player role dice 6 give chance again ,when consecutive 3 chance if greater go to next player...
+					if(consecutiveCount<3) {
+						consecutiveCount++;
+						i--;
+					}else {
+						consecutiveCount=0;
+					}
+				}
 			}
 		}
 	}
